25fcdc19ce6b9ab43edc2fe12329c2c8f3379d44
codejunkie75 <mhowland75@googlemail.com>
2018-03-04 11:40:42 +0000

    update

--- app/Http/Controllers/ip_addressController.php
+++ app/Http/Controllers/ip_addressController.php
@@ -4,13 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use Carbon\Carbon;
 
 class ip_addressController extends Controller
 {
   public function activity(){
+    $connectionsLastWeek = DB::table('ip_address')->where('created_at', '<', Carbon::now())
+    ->where('created_at', '>', Carbon::now()->subDays(29))
+    ->count();
+    $totalConnections = DB::table('ip_address')->count();
+    $bg = $this->connectionCount();
     $data = DB::table('ip_address')->paginate(100);
-    //return $ip;
-    return view('activity.activity', compact('data'));
+    return view('activity.activity', compact('data','bg'));
   }
-
+  public function timeIncrements(){
+    $array = array();
+    $now =Carbon::Now();
+    $dt = Carbon::create(2018, 1, 1, 0);
+    //return $dt->addMonths(2);
+    $x = 0;
+    while($dt < $now){
+      $dt = Carbon::create(2018, 1, 1, 0);
+      $dt = $dt->addMonths($x);
+       $array[] = $dt;
+       $x++;
+    }
+    return $array;
+  }
+  public function connectionCount(){
+    $dates = $this->timeIncrements();
+    $array = array();
+    $first = 0;
+    foreach ($dates as $date) {
+      if(!$first == 0){
+        $connections = DB::table('ip_address')->where('created_at', '<', $date)
+        ->where('created_at', '>', $first)
+        ->count();
+        $first->month = $first->month - 1;
+        $m = $first->year.', '.$first->month;
+        $array[] = array($connections,$m);
+      }
+      $first = $date;
+    }
+    return $array;
+  }
 }
--- resources/views/activity/activity.blade.php
+++ resources/views/activity/activity.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.backend')
 @section('content')
 <div class="row">
-  <div class="col-sm-6 col-sm-offset-3">
+  <div class="col-sm-3">
     <table class="table table-striped">
       <thead>
         <tr>
@@ -20,7 +20,39 @@
     </table>
     {{ $data->links() }}
   </div>
+  <div class="col-sm-9">
+      <div id="line_top_x"></div>
+  </div>
 </div>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
 
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'Month');
+      data.addColumn('number', 'Connections');
+      data.addRows([
+        @foreach ($bg as $x)
+          [new Date({{$x[1]}}),{{$x[0]}}],
+        @endforeach
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Connections',
+          subtitle: ''
+        },
+        width: 900,
+        height: 500,
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+  </script>
 
 @endsection
