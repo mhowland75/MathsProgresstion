18977802e014f172ca83817794f16dba32f6a9f7
mhowland75 <39522564+mhowland75@users.noreply.github.com>
2019-01-29 19:21:04 +0000

    no message

--- app/Http/Controllers/testingController.php
+++ app/Http/Controllers/testingController.php
@@ -2,6 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use App\Answer;
+use App\Question;
+use App\StudentLogin;
+use App\StudentsResult;
+use App\StudentsAnswer;
+use App\StudentYear;
+use App\Student;
+use App\Subject;
+use App\Test;
+
 class testingController extends Controller
 {
 
