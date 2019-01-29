0716c5a4839ea7653208ae8f863d75818be6c283
mhowland75 <39522564+mhowland75@users.noreply.github.com>
2019-01-29 19:02:54 +0000

    no message

--- app/Http/Controllers/testingController.php
+++ app/Http/Controllers/testingController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Subject;
 
 class testingController extends Controller
 {
     public function test(){
-      return 'test';
+      $subj = Subject::getSubjects();
+      $array = array();
+      foreach($subj as $sub){
+        $s = Student::where('student_id',StudentLogin::get_student_id())->get();
+        $results = StudentsResult::studentResults($s,$sub);
+        $array[$sub->subject] = $results;
+      }
+      return $array;
+      //return view('tests.studentResults.results',compact('tests','nav','year_id','course','array'));
     }
 }
